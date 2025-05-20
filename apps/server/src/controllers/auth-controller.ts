17926c818febf0220bd2de4d4160d0759bbbbac6
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2025-05-20 08:40:17 +0330

    feat: Add /me route and improve login route (main)
    Added a new route `/me` to retrieve the currently
    authenticated user's information. Improved the
    `/login` route to handle authentication more
    robustly.  Updated code formatting for better
    readability.

--- apps/server/src/controllers/auth-controller.ts
+++ apps/server/src/controllers/auth-controller.ts
@@ -1,24 +1,37 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import bcrypt from 'bcryptjs';
-import { generateToken } from '../utils/jwt';
+import {generateToken} from '../utils/jwt';
 import {User} from "../models/User";
+import {AuthRequest} from "../middlewares/auth-middleware";
 
 export class AuthController {
     static async login(req: Request, res: Response) {
-        const { username, password } = req.body;
+        const {username, password} = req.body;
 
-        const user = await User.findOne({ where: { username } });
+        const user = await User.findOne({where: {username}});
 
         if (!user) {
             return res.status(404).json({message: 'User does not exist'});
         }
 
         if (username !== user.username || !bcrypt.compareSync(password, user.password)) {
-            return res.status(401).json({ message: 'Invalid credentials' });
+            return res.status(401).json({message: 'Invalid credentials'});
         }
 
-        const token = generateToken({ id: user.id, username: user.username, role_id: user.role_id, is_super_admin: user.is_super_admin });
+        const token = generateToken({
+            id: user.id,
+            username: user.username,
+            role_id: user.role_id,
+            is_super_admin: user.is_super_admin
+        });
 
-        res.json({ token });
+        res.json({token});
+    }
+
+    static async me(req: AuthRequest, res: Response) {
+
+        const user = await User.findOne({where: {id: req.user.id}, attributes: {exclude: ['password']}});
+
+        return res.status(200).json(user);
     }
 }
--- apps/server/src/routes/auth.ts
+++ apps/server/src/routes/auth.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import {AuthController} from "../controllers/auth-controller";
+import {authenticateJWT} from "../middlewares/auth-middleware";
 
 const router = express.Router();
 
 router.post('/login', AuthController.login as any)
+router.get('/me', authenticateJWT, AuthController.me as any)
 
 
 export default router;
